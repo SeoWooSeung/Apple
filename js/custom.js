cff74df3c1a0e3d595b242cce9ba19eba2ef4179
서우승 <148739245+SeoWooSeung@users.noreply.github.com>
2024-02-23 18:19:46 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(function () {
+  $(".promo_slide-").slick({
+    arrows: false,
+    autoplay: true,
+    pauseOnHover: false,
+    dots: true,
+    // fade: true,
+  });
+
+  $(".img_slide").slick({
+    arrows: false,
+    autoplay: false,
+    pauseOnHover: false,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
+  $(".main_visual .arrows .left").on("click", function () {
+    $(".main_slide").slick("slickPrev");
+  });
+
+  $(".main_visual .arrows .right").on("click", function () {
+    $(".main_slide").slick("slickNext");
+  });
+
+  $(".mopen").on("click", function () {
+    $(".gnb").toggleClass("on");
+  });
+
+  $(".MainVisual").on("wheel", function (e) {
+    if ($(".itm").hasClass("on")) e.preventDefault();
+  });
+
+  $(".mopen").on("click", function () {
+    $(".header").toggleClass("active");
+    // $(".icon").toggleClass("on");
+  });
+});
